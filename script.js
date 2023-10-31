16a2bef114cdcd40795071cde6a01b316d55511b
Liebe974 <137163247+Liebe974@users.noreply.github.com>
2023-10-31 11:48:17 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const form = document.querySelector("form");
+const container = document.querySelector(".container");
+const taskInput = document.querySelector("#taskInput");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  //   Méthode 1
+  //   container.innerHTML += `
+  //     <div class="task">
+  //       <p>Lorem, ipsum dolor</p>
+  //       <button>delete</button>
+  //     </div>`;
+
+  // Méthode 2
+  const task = document.createElement("div");
+  task.classList.add("task");
+  const p = document.createElement("p");
+  const button = document.createElement("button");
+  button.innerText = "delete";
+
+  task.appendChild(p);
+  p.innerText = taskInput.value;
+  task.appendChild(button);
+  container.appendChild(task);
+});
+
+// clear input after submit
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  taskInput.value = "";
+})
+// Delete task individually
+
+
+// TODO : add task to cache (local storage) : persist data
